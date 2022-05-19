f165a9067223bf1af7f583730156e66b2c811e3e
NagaiKoki <kokinagai10+1@gmail.com>
2022-05-19 22:54:33 +0900

    add smaple

--- demo/index.ts
+++ demo/index.ts
@@ -74,3 +74,45 @@ type SampleBaseJsonApiType = JsonApiDeserializeType<ApiType>;
 type SampleArrayJsonApiType = JsonApiDeserializeType<ArrayApiType>;
 type SampleRelationalJsonApiType =
   JsonApiRelationshipDeserializeType<RelationShipsApiType>;
+
+const Sample1: SampleBaseJsonApiType = {
+  name: "Koki Nagai",
+  imageUrl: "https://avatars.githubusercontent.com/u/50698194?v=4",
+  age: 26,
+};
+
+const Sample2: SampleArrayJsonApiType = [
+  {
+    name: "Koki Nagai",
+    imageUrl: "https://avatars.githubusercontent.com/u/50698194?v=4",
+    age: 26,
+  },
+  {
+    name: "Koki Nagai2",
+    imageUrl: "https://avatars.githubusercontent.com/u/50698194?v=4",
+    age: 21,
+  },
+];
+
+const Sample3: SampleRelationalJsonApiType = {
+  name: "Nagai Koki",
+  drinks: [
+    {
+      name: "Coke",
+      imageUrl: "https://avatars.githubusercontent.com/u/50698194?v=4",
+      createdAt: "2021/01/01",
+    },
+  ],
+  books: [
+    {
+      title: "TDD",
+      imageUrl: "https://avatars.githubusercontent.com/u/50698194?v=4",
+      publishedAt: "2021/01/01",
+    },
+    {
+      title: "DDD",
+      imageUrl: "https://avatars.githubusercontent.com/u/50698194?v=4",
+      publishedAt: "2021/01/02",
+    },
+  ],
+};
